221564e3c553213d55487ea51c8d62667d543399
netsujoupark <adamas910@naver.com>
2019-10-11 22:52:15 +0900

    Good Time

--- PARKMS1/src/GridLayoutEx.java
+++ PARKMS1/src/GridLayoutEx.java
@@ -1,53 +1,39 @@
-
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.util.Calendar;
 
-import javax.swing.*;
-
-public class GridLayoutEx extends JFrame implements ActionListener {
-	
-	public static JLabel label_time = new JLabel();
-	public static Calendar now = Calendar.getInstance();
-	
+public class GridLayoutEx extends JFrame {
 	public GridLayoutEx() {
-		this.setTitle("What time is it now?");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c =this.getContentPane();
-		c.setLayout(null);
+		setTitle("Good ~~");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		label_time.setBounds(150,20,200,50);
-		c.add(label_time);
+		GridLayout grid = new GridLayout(1,1);  
 		
-		JButton button = new JButton("Time");
-		button.setBounds(20+180,100,100,30);
-		c.add(button);
-		button.addActionListener(this);
-		setSize(500,300);
+		Calendar cal = Calendar.getInstance();
+		
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		
+		String a ="";
+		if(hour<12) {
+			a="Good Morning";
+		}
+		if(hour>12 && hour<18) {
+			a="Good Afternoon";
+		}
+		if(hour>=18) {
+			a="Good Evening";
+		}
+		
+		Container c = getContentPane();
+		c.setLayout(grid);
+		c.add(new JLabel(a));
+		
+
+		setSize(300,200);
 		setVisible(true);
 	}
-
-		public static void main(String[] args) {
-			new GridLayoutEx();
-			
-		}
-
-		
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		JButton button = (JButton)e.getSource();
-		if(button.getText().equals("Time")) {
-			int hour = GridLayoutEx.now.get(Calendar.HOUR_OF_DAY);
-	        int min = GridLayoutEx.now.get(Calendar.MINUTE);
-	        int second = GridLayoutEx.now.get(Calendar.SECOND);
-	        
-	        
-	        GridLayoutEx.label_time.setText("Now Time is "+hour + " : " + min + " : " + second );
-		}
+	public static void main(String[] args) {
+		new GridLayoutEx();
 	}
 }
-
 
